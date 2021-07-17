3b0307e9984dd7259d7b4370e77aa22ecee36852
Kyle <Kyle@DESKTOP>
2021-07-17 17:54:18 +1000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassImageRenamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectImageNames = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse csv files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "csv",
+                Filter = "csv files (*.csv)|*.csv",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog imageLocation = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.MyComputer
+            };
+
+            if (imageLocation.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = imageLocation.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog moveLocation = new FolderBrowserDialog
+            {
+                RootFolder = Environment.SpecialFolder.MyComputer
+            };
+
+            if (moveLocation.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = moveLocation.SelectedPath;
+            }
+        }
+    }
+}
